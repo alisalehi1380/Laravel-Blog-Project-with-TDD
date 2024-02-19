65ea194afadb06f8b990946a0b616fcf6347a671
Ali Salehi <alisalehi13800930@gmail.com>
2024-02-19 22:14:34 +0330

    Refactor(requests)

--- app/Http/Controllers/Auth/Requests/RegisterNormallUserRequest.php
+++ app/Http/Controllers/Auth/Requests/RegisterNormallUserRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Controllers\Auth\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class RegisterNormallUserRequest extends FormRequest
+class RegisterRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
